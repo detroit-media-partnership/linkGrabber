156665db57c4a34b8cf3ce6a8c97f976defbe382
Eric Bower <neurosnap@gmail.com>
2014-05-16 08:38:22 -0700

    updated find_links to separate functionality

--- linkGrabber/links.py
+++ linkGrabber/links.py
@@ -1,8 +1,9 @@
 class Links:
     """Object that will hold link data"""
-    def __init__(self, text, href):
+    def __init__(self, text, href, seo):
         self.text = text
         self.href = href
+        self.seo = seo
 
     def __repr__(self):
         return "<Links text=%r, href=%r>" % (self.text, self.href)
--- linkGrabber/scrape.py
+++ linkGrabber/scrape.py
@@ -4,34 +4,43 @@ import urllib2
 from BeautifulSoup import BeautifulSoup
 from .links import Links
 
-
 class ScrapeLinks:
     """Grabs links from a web page
     based upon a URL, filters, and limits"""
     def __init__(self, href):
         self._href = href
         self._links = []
+        self._soup = None
 
     def __repr__(self):
         return "<ScrapeLinks %r>" % self._href
 
-    def _add_link(self, text, href):
-        self._links.append(Links(text, href))
+    def _add_link(self, text, href, seo):
+        self._links.append(Links(text, href, seo))
+
+    def _get_page(self):
+        page = urllib2.urlopen(self._href)
+        self._soup = BeautifulSoup(page)
+        return self._soup
 
     def find_links(self, filters=None, limit=None):
-        page = urllib2.urlopen(self._href)
-        soup = BeautifulSoup(page)
+        if self._soup is None:
+            self._get_page()
         if filters is not None:
-            search = soup.findAll('a', **filters)
+            search = self._soup.findAll('a', **filters)
         else:
-            search = soup.findAll('a')
+            search = self._soup.findAll('a')
         for anchor in search:
             link_href = anchor['href']
             last_slash = anchor['href'].rfind('/')
-            link_text = anchor['href'][last_slash+1:] \
+            link_seo = anchor['href'][last_slash+1:] \
                         .replace('-', ' ') \
                         .replace('  ', ' ')
-            self._add_link(link_text, link_href)
+            try:
+                link_text = anchor.string
+            except:
+                link_text = link_seo
+            self._add_link(link_text, link_href, link_seo)
             if limit is not None and len(self._links) >= limit:
                 break
         return self._links
