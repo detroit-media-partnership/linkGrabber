728417fadc2c68019e867c2c1fd2faa8e14a5fa3
mikewaters <robert.waters@gmail.com>
2014-05-18 00:43:51 -0400

    Code review for tests.

--- linkGrabber/tests/test_scrape.py
+++ linkGrabber/tests/test_scrape.py
@@ -22,10 +22,21 @@ class TestScrape(unittest.TestCase):
     def test_find(self):
         """ Test how grabbing the hyperlinks are aggregated """
         seek = Links(self.url)
+        # Each of these assertions should have their own test method.
         self.assertRaises(Exception, seek.find, filters=['href', 'style'])
+        # ex: "test_find_hyperlink_bad_filter_param"
         self.assertRaises(Exception, seek.find, filters=25)
         self.assertEqual(len(seek.find(limit=5)), 5)
         self.assertEqual(len(seek.find(limit=1)), 1)
 
 if __name__ == '__main__':
+    # You should instead gain experience with a test runner.
+    # They will discover your unit tests, and run them.
+    # py.test, the latest-and-greatest
+    # http://pytest.org/latest/
+    # Nose, very popular
+    # https://nose.readthedocs.org/en/latest/
+    # Also consider tox, if you want to run your tests
+    # on both py2 and 3 (which I think you do, judging by scrape.py):
+    # http://tox.readthedocs.org/en/latest/
     unittest.main(verbosity=2)
