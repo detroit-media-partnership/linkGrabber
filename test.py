bf331611576543edd1a319cd7b701b75e345cb70
Eric Bower <neurosnap@gmail.com>
2014-06-25 14:55:11 -0400

    removed test file

--- test.py
+++
@@ -1,9 +0,0 @@
-import re
-
-from linkGrabber import Links
-
-
-links = Links("http://www.detroitnews.com/section/NLETTER04")
-links.find(limit=2, duplicates=False, pretty=True,
-			href=re.compile("www.detroitnews.com/article"),
-			exclude=[{"href":re.compile("99999999")}])
